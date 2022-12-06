ffc1acf6303bd702f3c64012a1dc58ff04b8c79b
GMShimon <simon.rosedale99@gmail.com>
2022-12-06 16:47:02 +0800

    new style with expanded

--- lib/main.dart
+++ lib/main.dart
@@ -23,38 +23,52 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[700],
         centerTitle: true,
       ),
-      body: Row(children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Image(
-              image: NetworkImage(
-                  'https://i.natgeofe.com/n/2a12edda-3f79-45d1-b9bc-7ccc1499e5cf/1230_16x9.jpg?w=1200')),
-        ),
-        Expanded(
-          flex: 3,
-          child: Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.cyan,
-            child: Text("1"),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(17.0),
+              color: Colors.green[700],
+              child: Text(
+                "hello",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.yellow,
-            child: Text("2"),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(17.0),
+              color: Colors.red[700],
+              child: Text(
+                "World",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.blue,
-            child: Text("3"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(17.0),
+              color: Colors.green[900],
+              child: Text(
+                "!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
           ),
-        )
-      ]),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print("Hello world!");
